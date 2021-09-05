ab888b97b2297540b96240ae6ecbef0ce9f739f2
Akshay3960 <akshay3960@gmail.com>
2021-09-05 23:22:36 +0530

    Registration Modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'LogInPage/login_page.dart';
-import './subject_page/subject_page.dart';
 import './menuBar/menuBarScreens/fees.dart';
 import './menuBar/menuBarScreens/profile.dart';
 import './menuBar/menuBarScreens/registration/registration.dart';
@@ -20,6 +19,11 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme:  ThemeData(
         primarySwatch: Colors.amber,
+        // primaryColor: Color.fromRGBO(200, 100, 100, 1),
+        // scaffoldBackgroundColor: Color.fromRGBO(0, 0, 0, 1),
+        // brightness: Brightness.dark,
+        // appBarTheme: AppBarTheme(color: Color.fromRGBO(150, 200, 10, 1)),
+        // appBarTheme: AppBarTheme(color: Color.fromRGBO(150, 10, 50, 1)),
       ),
       home: LogInPage(),
 
